b70b151a4136d821a5399faa8fd8cb9ad8b5bcb1
njain51 <official.nitinjain@gmail.com>
2024-03-30 11:48:46 -0400

    file system example code for reading text file

--- 3_FS_example/index.js
+++
@@ -1,9 +0,0 @@
-// imported FS module
-import FS from 'fs';
-import  lib, { sum, diff } from './lib.js';
-
-// reading file in synchronous moded and adding encoding for display else default output in byte only
-const txt = FS.readFileSync('demo.txt', "utf8");
-console.log(txt)
-
-console.log(sum(4,5),diff(9,4));
--- 3_FS_example/lib.js
+++
@@ -1,9 +0,0 @@
-export function diff (a,b)  {
-    return a-b;
-}
-
-export function sum(a,b) {
-    return a+b;
-}
-
-export default 'lib'
